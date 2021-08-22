a233db6c4fc25d365d338c54ff9b14695106026b
Marta Kruk <kruk.marta@outlook.com>
2021-08-22 19:25:35 +0200

    7.2. Czym jest Stream?

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -27,7 +28,9 @@ public class StreamMain {
         poemBeautifier.beautify("There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy.", (poem) -> "ABC" + poem + "ABC");
         poemBeautifier.beautify("There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy.", (poem) -> poem.toUpperCase());
         poemBeautifier.beautify("There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy.", (poem) -> "*~* " + poem.toLowerCase() + " *~*");
-        poemBeautifier.beautify("There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy.", (poem) -> "\n" + poem);
+        poemBeautifier.beautify("There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy.", (poem) -> "\n" + poem + "\n");
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }

ba588e6cd9e0a6b0650153a61219944c1d62877f
Marta Kruk <kruk.marta@outlook.com>
2021-08-26 12:52:56 +0200

    8.2. try! catch! finally!

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,30 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            FirstChallenge firstChallenge = new FirstChallenge();
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        }
+        catch (Exception ArithmeticException) {
+            System.out.println("Nie dziel przez zero!");
+        }
+        finally {
+            System.out.println("Koniec");
+        }
+    }
+
+}

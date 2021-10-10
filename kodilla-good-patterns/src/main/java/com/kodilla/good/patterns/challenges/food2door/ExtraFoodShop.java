5dc9a2bc11f895e0f722bcb6a6377e9e5f1d4dcc
Marta Kruk <kruk.marta@outlook.com>
2021-10-10 14:58:47 +0200

    13.4

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/ExtraFoodShop.java
@@ -0,0 +1,9 @@
+package com.kodilla.good.patterns.challenges.food2door;
+
+public class ExtraFoodShop implements Supplier {
+
+    @Override
+    public void process(Product product, int quantity) {
+        System.out.println("Product " + product.getName() + " (" + quantity + ") will be delivered to your door! Thank you for shopping with ExtraFoodShop.");
+    }
+}

c1d7d3337184f5ed180e5496f64a6e1d39be7d0e
Marta Kruk <kruk.marta@outlook.com>
2021-11-07 15:44:22 +0100

    17.4

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
@@ -13,6 +16,8 @@ class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -55,6 +60,57 @@ class CompanyDaoTestSuite {
             companyDao.deleteById(softwareMachineId);
             companyDao.deleteById(dataMaestersId);
             companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+    @Test
+    void testNamedQueries() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee eveSmith = new Employee("Eve", "Smith");
+        Employee monicaGeller = new Employee("Monica", "Geller");
+        Employee chandlerBing = new Employee("Chandler", "Bing");
+
+        Company transponster = new Company("Transponster");
+        Company dataAnalytics = new Company("Data Analytics");
+        Company alessandros = new Company("Alessandro's");
+
+        transponster.getEmployees().add(chandlerBing);
+        transponster.getEmployees().add(eveSmith);
+        dataAnalytics.getEmployees().add(chandlerBing);
+        dataAnalytics.getEmployees().add(johnSmith);
+        alessandros.getEmployees().add(monicaGeller);
+        alessandros.getEmployees().add(eveSmith);
+
+        chandlerBing.getCompanies().add(transponster);
+        chandlerBing.getCompanies().add(dataAnalytics);
+        johnSmith.getCompanies().add(dataAnalytics);
+        monicaGeller.getCompanies().add(alessandros);
+        eveSmith.getCompanies().add(alessandros);
+        eveSmith.getCompanies().add(dataAnalytics);
+
+        companyDao.save(transponster);
+        int transponsterId = transponster.getId();
+        companyDao.save(dataAnalytics);
+        int dataAnalyticsId = dataAnalytics.getId();
+        companyDao.save(alessandros);
+        int alessandrosId = alessandros.getId();
+
+        //When
+        List<Employee> employeesByLastname = employeeDao.retrieveEmployeesByLastname("Smith");
+        List<Company> companiesByFirstThreeCharactersOfName = companyDao.retrieveCompaniesByFirstThreeCharactersOfName("Tra");
+
+        //Then
+        assertEquals(2, employeesByLastname.size());
+        assertEquals(1, companiesByFirstThreeCharactersOfName.size());
+
+        //CleanUp
+        try {
+            companyDao.deleteById(transponsterId);
+            companyDao.deleteById(dataAnalyticsId);
+            companyDao.deleteById(alessandrosId);
         } catch (Exception e) {
             //do nothing
         }

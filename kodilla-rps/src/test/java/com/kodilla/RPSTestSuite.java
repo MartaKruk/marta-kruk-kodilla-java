1c7704f973b850c00523167c6212a285ca92bdcb
Marta Kruk <kruk.marta@outlook.com>
2021-11-21 17:34:50 +0100

    Added RPSLSRules

--- kodilla-rps/src/test/java/com/kodilla/RPSTestSuite.java
+++ kodilla-rps/src/test/java/com/kodilla/RPSTestSuite.java
@@ -9,13 +9,45 @@ public class RPSTestSuite {
     @Test
     void testRPSRules() {
         //Given
-        int player1Move = 1;
-        int player2Move = 2;
+        int rock = 1;
+        int paper = 2;
+        int scissors = 3;
         //When
         RPSRules rpsRules = new RPSRules();
-        int winner = rpsRules.whoWon(player1Move, player2Move);
+        int winner1 = rpsRules.whoWon(rock, scissors);
+        int winner2 = rpsRules.whoWon(scissors, paper);
+        int winner3 = rpsRules.whoWon(paper, rock);
         //Then
-        assertEquals(2,winner);
+        assertEquals(1, winner1);
+        assertEquals(1, winner2);
+        assertEquals(1, winner3);
+    }
+
+    @Test
+    void testRPSLSRules() {
+        //Given
+        int rock = 1;
+        int paper = 2;
+        int scissors = 3;
+        int lizard = 4;
+        int spock = 5;
+        //When
+        RPSLSRules rpslsRules = new RPSLSRules();
+        int winner1 = rpslsRules.whoWon(rock, lizard);
+        int winner2 = rpslsRules.whoWon(lizard, spock);
+        int winner3 = rpslsRules.whoWon(spock, scissors);
+        int winner4 = rpslsRules.whoWon(scissors, lizard);
+        int winner5 = rpslsRules.whoWon(lizard, paper);
+        int winner6 = rpslsRules.whoWon(paper, spock);
+        int winner7 = rpslsRules.whoWon(spock, rock);
+        //Then
+        assertEquals(1, winner1);
+        assertEquals(1, winner2);
+        assertEquals(1, winner3);
+        assertEquals(1, winner4);
+        assertEquals(1, winner5);
+        assertEquals(1, winner6);
+        assertEquals(1, winner7);
     }
 
     @Test
